e78d02a8257da3e4966d20b12e3c4e25e3705794
ketheres <mitja.salo@hotmail.com>
2017-03-01 13:53:37 +0200

    GDS complete, working screen handling
    GDS done
    Also you can now switch between main menu, cellar, hole and window. Yay!

--- Plantastic/Assets/Scripts/GameManager.cs
+++ Plantastic/Assets/Scripts/GameManager.cs
@@ -4,18 +4,27 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
     [Header("Header")]
     [Tooltip("Tooltip")]
-    public Chapter[]    chapters;
-    public int          currentChapter;
-    public GameObject[] inventory;
-    public Card[]       cards;
-    public int          day;
-    public TurnPhase    turnPhase;
-    public Animator     animator;
+    public Chapter[]        chapters;
+    public int              currentChapter;
+    public int              currentTurn;
+    public GameObject[]     inventory;
+    public Card[]           plantDeck;
+    public Card[]           eventDeck;
+    public int              day;
+    public TurnPhase        turnPhase;
+    public Animator         animator;
+    public GameObject       BGHolder;
+    public SpriteRenderer   BGSprite;
+    public Sprite           menuBG;
+    public Sprite           cellarBG;
+    public Sprite           holeBG;
+    public Sprite           windowBG;
 	// Use this for initialization
 	void Start ()
     {
         inventory = GameObject.FindGameObjectsWithTag("Item");
-        
+        BGHolder = GameObject.FindGameObjectWithTag("BG");
+        BGSprite = BGHolder.GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
@@ -30,4 +39,11 @@ public enum TurnPhase
     PLANT,
     DECK,
     BUG
+}
+public enum MainState
+{
+    MAIN_MENU,
+    CELLAR,
+    HOLE,
+    WINDOW
 }
--- Plantastic/Assets/Scripts/SceneBehaviour.cs
+++ Plantastic/Assets/Scripts/SceneBehaviour.cs
@@ -1,19 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SceneBehaviour : StateMachineBehaviour
+public class SceneBehaviour : MonoBehaviour
 {
-    public enum MainState
-    {
-        MAIN_MENU,
-        CELLAR,
-        HOLE,
-        WINDOW
-    }
+    /* public enum MainState
+     {
+         MAIN_MENU,
+         CELLAR,
+         HOLE,
+         WINDOW
+     }*/
     // Use this for initialization
+    public Animator anim;
     void Start ()
     {
-
+        //anim = gameObject.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
